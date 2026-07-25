9eadb7b11dc12c8a7fba577426482e4769de9bd8
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 16:24:45 +0530

    Add string sorting program in Program32.c

---
+++ Program32.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], temp;
+    int i, j;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; i < strlen(str)-1; i++) {
+        for(j = i + 1; j < strlen(str); j++) {
+            if(str[i] > str[j]) {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+
+    printf("Ascending = %s", str);
+
+    return 0;
+}
