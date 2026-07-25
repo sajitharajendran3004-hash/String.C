b9e263a2653100a6aeffdaaefef176d89a80fedc
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 16:09:01 +0530

    Add program to count words in a sentence

---
+++ program18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    scanf("%[^\n]", str);
+
+    while(str[i] != '\0') {
+        if(str[i] == ' ')
+            words++;
+        i++;
+    }
+
+    printf("Words = %d", words);
+
+    return 0;
+}
