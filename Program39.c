a18f798f850bed9b26f338cbe3c7823d11228616
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 19:27:36 +0530

    Add program to count uppercase and lowercase letters

---
+++ Program39.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i=0, upper=0, lower=0;
+
+    printf("Enter string: ");
+    scanf("%[^\n]",str);
+
+    while(str[i]!='\0'){
+        if(str[i]>='A'&&str[i]<='Z')
+            upper++;
+        else if(str[i]>='a'&&str[i]<='z')
+            lower++;
+
+        i++;
+    }
+
+    printf("Uppercase = %d\n",upper);
+    printf("Lowercase = %d",lower);
+
+    return 0;
+}
